79198b27fa8791fedbfb8425ffb0b36cdf45f71a
AlexandrePadilhaa <105119507+AlexandrePadilhaa@users.noreply.github.com>
2024-01-05 15:38:59 -0300

    excluindo template

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function Contatos() {
+    return (
+        <div>
+            <h1>Pagina contatos</h1>
+            <Link href='/'>
+                <h2>Ir para home</h2>
+            </Link>
+        </div>)
+}
